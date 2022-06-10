f4f55d37a8c880a37dd8039fdf6b2c126245b539
Shashank Acharya <acharya.s@icloud.com>
2022-06-10 14:33:04 +0800

    Create DashboardPage.js

---
+++ src/pages/DashboardPage/DashboardPage.js
@@ -0,0 +1,10 @@
+import { Container, SimpleGrid, Stack, Title } from "@mantine/core";
+import NFTSummaryCard from "../../components/dashboard/NFTSummaryCard/NFTSummaryCard";
+
+export default function DashboardPage() {
+	return (
+		<Stack p='lg'>
+			<Title>Dashboard</Title>
+		</Stack>
+	)
+}
