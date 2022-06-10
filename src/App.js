1b0a97d4f7d72d14a55919bfbbc506846a8a991d
Shashank Acharya <acharya.s@icloud.com>
2022-06-10 14:32:36 +0800

    moved contents to dashboard

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Container, Title } from "@mantine/core";
+import { Title } from "@mantine/core";
+import AppContainer from "./components/navigation/AppContainer/AppContainer";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
 
 export default function App() {
 	return (
-		<Container p='lg'>
-			<Title>Welcome to React App</Title>
-		</Container>
+		<AppContainer>
+			<DashboardPage/>
+		</AppContainer>
 	);
 }
