aa03662c4ae03722a8fd56451b3a773a0ce6403b
Shashank Acharya <acharya.s@icloud.com>
2022-06-10 16:14:48 +0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Title } from "@mantine/core";
+import { Route, Routes } from "react-router-dom";
 import AppContainer from "./components/navigation/AppContainer/AppContainer";
+import CustomersPage from "./pages/CustomersPage/CustomersPage";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import MerchantsPage from "./pages/MerchantsPage/MerchantsPage";
+import TokensPage from "./pages/TokensPage/TokensPage";
+import TransactionsPage from "./pages/TransactionsPage/TransactionsPage";
 
 export default function App() {
 	return (
 		<AppContainer>
-			<DashboardPage/>
+			<Routes>
+				<Route path="/" element={<DashboardPage/>}/>
+				<Route path="/tokens" element={<TokensPage/>}/>
+				<Route path="/customers" element={<CustomersPage/>}/>
+				<Route path="/merchants" element={<MerchantsPage/>}/>
+				<Route path="/transactions" element={<TransactionsPage/>}/>
+			</Routes>
 		</AppContainer>
 	);
 }
